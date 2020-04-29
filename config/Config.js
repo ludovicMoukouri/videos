5eb4af2393419086fda83f257803accea96a85ed
Ludovic Moukouri <ludovicmoukouri@yahoo.fr>
2020-04-29 11:07:24 +0200

    WebSocket(HOST) setting

--- config/Config.js
+++ config/Config.js
@@ -1,8 +1,10 @@
 module.exports = {
 DB: 'mongodb+srv://ludovic:MludMongodb16@cluster0-tpowy.mongodb.net/test?retryWrites=true&w=majority',
 SECRET: 'movieratingapplicationsecretkey',
-FACEBOOK_APP_ID: '665542867611115',
-FACEBOOK_APP_SECRET: '4589a8da15955a11d18d0dcf35f3d597',
+// FACEBOOK_APP_ID: '665542867611115', for moiledoux@yahoo.fr
+// FACEBOOK_APP_SECRET: '4589a8da15955a11d18d0dcf35f3d597', for moiledoux@yahoo.fr
+FACEBOOK_APP_ID: '224070629040899',
+FACEBOOK_APP_SECRET: 'd828c38f793fe1cdbd753baad00f4a89',
 GOOGLE_APP_ID: '860816668971-6d4j0eleraj15ne36sco85ni2qd00lhj.apps.googleusercontent.com',
 GOOGLE_APP_SECRET: 'cvPdwuTOfOFK9nXGafmz_qVN',
 }
