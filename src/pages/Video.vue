e81daf039db1e432cb77f8c2f98e3192db725cc6
Ludovic Moukouri <ludovicmoukouri@yahoo.fr>
2020-05-06 13:13:08 +0200

    Configure fetching methode changing in created() hook 3

--- src/pages/Video.vue
+++ src/pages/Video.vue
@@ -255,6 +255,7 @@ export default {
   },
   listenToLogout() {
     bus.$on('refreshLogout', () => {
+      window.location.reload()
       this.$store.dispatch("sendAction", { type: 'leave' });
     });
   },
