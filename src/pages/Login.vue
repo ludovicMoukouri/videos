691a05bf9001f4f4412b906ff36dd1357544bf1c
Ludovic Moukouri <ludovicmoukouri@yahoo.fr>
2020-05-11 15:16:21 +0200

    add loadresponsive methode in mounted in Video Page 4

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -141,7 +141,7 @@ export default {
       return true;
     },
     loadresponsive() {
-      return this.$router.go()
+      return this.$router.go(1)
     },
     clear() {
       this.$refs.form.reset();
